533a417762c7ca5146da1013d250cab4eda839bd
Lilly <lgr4641@rit.edu>
2024-03-02 18:44:26 -0500

    more udoate

--- degree_by_gender_analysis.py
+++ degree_by_gender_analysis.py
@@ -2,15 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read data from Excel file
-df = pd.read_excel("assets/Degree_By_Gender_200813.xlsx")
-
+df_total = pd.read_excel("assets/Degree_By_Gender_200812.xlsx", sheet_name="Total")
+df_male = pd.read_excel("assets/Degree_By_Gender_200812.xlsx", sheet_name="Male")
+df_female = pd.read_excel("assets/Degree_By_Gender_200812.xlsx", sheet_name="Female")
+print(df_total)
+print(df_male)
+print(df_female)
 # Set index to 'Field of Study'
-df.set_index('Field of Study', inplace=True)
-
+df_total.set_index('Field of Study', inplace=True)
+columns = columns = [
+    "Attained bachelor's degree",
+    "Attained associate's degree",
+    "Attained certificate",
+    "No degree, still enrolled",
+    "No degree, not enrolled"
+]
 # Plot the bar graph
-ax = df[['Attained bachelor\'s degree (Total)', 'Attained associate\'s degree (Total)',
-         'Attained certificate (Total)', 'No degree, still enrolled (Total)',
-         'No degree, not enrolled (Total)']].plot(kind='bar', figsize=(12, 8), colormap='Paired')
+ax = df_total[columns].plot(kind='bar', figsize=(12, 8), colormap='Paired')
+ax = df_male[columns].plot(kind='bar', figsize=(12, 8), colormap='Paired')
+ax = df_female[columns].plot(kind='bar', figsize=(12, 8), colormap='Paired')
 
 # Add title and labels
 plt.title("Percentage of Attainment or Level at Last Institution Enrolled by Field of Study")
