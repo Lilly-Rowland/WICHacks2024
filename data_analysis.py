e4fa29701fddffd8c2b3ee96952caa3c074b098e
Lilly <lgr4641@rit.edu>
2024-03-02 15:35:53 -0500

    synced flask python with website tabs

--- data_analysis.py
+++ data_analysis.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/data.html')
+def data():
+    return render_template('data.html', the_title='data')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html', the_title='about')
+
+@app.route('/resources.html')
+def resources():
+    return render_template('resources.html', the_title='resources')
