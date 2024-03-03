42d6fe87565626ce5837e00392fffc608e1d0ce3
Lilly <lgr4641@rit.edu>
2024-03-03 00:14:13 -0500

    sql create table updated for the survery

--- survey_results.sql
+++ survey_results.sql
@@ -3,7 +3,10 @@ DROP TABLE IF EXISTS Response;
 CREATE TABLE Response (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    name VARCHAR(30) NOT NULL,
-    age SMALLINT NOT NULL,
-    feedback VARCHAR(500) NOT NULL
+    year ENUM('1', '2', '3', '4+') NOT NULL,
+    major VARCHAR(50),
+    confidence INT CHECK (confidence >= 1 AND confidence <= 10),
+    imposter ENUM('yes', 'no'),
+    support ENUM('Classmates', 'Professors', 'Friends', 'Family', 'Extracurriculars', 'Other'),
+    feedback VARCHAR(400)
 );
