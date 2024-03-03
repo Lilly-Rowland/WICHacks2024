799eb4127ecc0774956b296202a86b4940f0dc0b
Lilly <lgr4641@rit.edu>
2024-03-03 08:08:42 -0500

    updated table

--- survey_results.sql
+++ survey_results.sql
@@ -1,12 +1,14 @@
+
+
 DROP TABLE IF EXISTS Response;
 
 CREATE TABLE Response (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    year ENUM('1', '2', '3', '4+') NOT NULL,
-    major VARCHAR(50),
+    year TEXT CHECK (year IN ('1', '2', '3', '4+')) NOT NULL,
+    major VARCHAR(50) NOT NULL,
     confidence INT CHECK (confidence >= 1 AND confidence <= 10),
-    imposter ENUM('yes', 'no'),
-    support ENUM('Classmates', 'Professors', 'Friends', 'Family', 'Extracurriculars', 'Other'),
+    imposter TEXT CHECK (imposter IN ('yes', 'no')),
+    support TEXT CHECK (support IN ('Classmates', 'Professors', 'Friends', 'Family', 'Extracurriculars', 'Other')),
     feedback VARCHAR(400)
 );
